b89d331ca410581c974bd1c6b107cdeb0c08518e
joonbo97 <joonbo97@naver.com>
2024-04-06 00:05:33 +0900

    일부 주석 처리

--- app/src/main/java/com/vecto_example/vecto/MapNotification.kt
+++ app/src/main/java/com/vecto_example/vecto/MapNotification.kt
@@ -7,6 +7,7 @@ import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import androidx.core.app.NotificationCompat
 
 object MapNotification {
@@ -24,7 +25,7 @@ object MapNotification {
         val notification = NotificationCompat.Builder(context, CHANNEL_ID)
             .setContentTitle("위치정보를 수집중입니다.")
             .setContentText("백그라운드에서 작업을 수행중입니다.")
-            .setContentInfo("Info 입니다")
+            //.setContentInfo("Info 입니다")
             .setSmallIcon(R.drawable.notification_icon)
             .setOngoing(true)//사라지지 않는 알림으로 만들기
             .setContentIntent(pendingIntent)
