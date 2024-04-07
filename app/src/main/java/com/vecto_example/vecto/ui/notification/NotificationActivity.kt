a0bc4e786d45c431411d2ab30e0ae326d8066336
joonbo97 <joonbo97@naver.com>
2024-04-08 03:36:40 +0900

    코드 개선

--- app/src/main/java/com/vecto_example/vecto/ui/notification/NotificationActivity.kt
+++ app/src/main/java/com/vecto_example/vecto/ui/notification/NotificationActivity.kt
@@ -2,65 +2,108 @@ package com.vecto_example.vecto.ui.notification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.activity.OnBackPressedCallback
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.vecto_example.vecto.ui.notification.adapter.MyNotificationAdapter
 import com.vecto_example.vecto.data.Auth
 import com.vecto_example.vecto.data.model.NotificationDatabase
+import com.vecto_example.vecto.data.repository.NotificationRepository
 import com.vecto_example.vecto.databinding.ActivityNotificationBinding
+import com.vecto_example.vecto.retrofit.VectoService
 
 class NotificationActivity : AppCompatActivity() {
     lateinit var binding: ActivityNotificationBinding
-    lateinit var myNotificationAdapter: MyNotificationAdapter
+    private lateinit var myNotificationAdapter: MyNotificationAdapter
+
+    private val notificationViewModel: NotificationViewModel by viewModels {
+        NotificationViewModelFactory(NotificationRepository(VectoService.create()))
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityNotificationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initRecyclerView()
+        initObserver()
+        initListener()
+        getNotification()
+    }
+
+    private fun initListener() {
+        binding.BackButton.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun initObserver() {
+        /*   Notification 관련 Observer   */
+        notificationViewModel.notificationsLiveData.observe(this){
+            notificationViewModel.notificationsLiveData.value?.let {
+                myNotificationAdapter.addNotificationData(
+                    it.notifications)
+            }
+
+            if(notificationViewModel.allNotifications.isEmpty()){
+                setNoneImage()
+            }
+        }
+
+        /*   로딩 관련 Observer   */
+        notificationViewModel.isLoadingCenter.observe(this) {
+            if(it)
+                binding.progressBarCenter.visibility = View.VISIBLE
+            else
+                binding.progressBarCenter.visibility = View.GONE
+        }
+        notificationViewModel.isLoadingBottom.observe(this) {
+            if(it)
+                binding.progressBar.visibility = View.VISIBLE
+            else
+                binding.progressBar.visibility = View.GONE
+        }
+
+    }
+
+    private fun initRecyclerView() {
         myNotificationAdapter = MyNotificationAdapter(this)
         val notificationRecyclerView = binding.NotificationRecyclerView
         notificationRecyclerView.adapter = myNotificationAdapter
         notificationRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        notificationRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
 
-        if(Auth.loginFlag.value == true) {
-            val notifications = NotificationDatabase(this).getAllNotificationData()
-            for (i in 0 until notifications.size)
-                myNotificationAdapter.notificationData.add(notifications[i])
-        }
-
-        if(myNotificationAdapter.notificationData.isEmpty())
-        {
-            binding.NoneImage.visibility = View.VISIBLE
-            binding.NoneText.visibility = View.VISIBLE
-        }
-        else
-        {
-            binding.NoneImage.visibility = View.GONE
-            binding.NoneText.visibility = View.GONE
-        }
-
-        binding.BackButton.setOnClickListener {
-            finish()
-        }
-
-
-
-
-        val callback = object : OnBackPressedCallback(true /* enabled by default */) {
-            override fun handleOnBackPressed() {
-                val hasUnshown = NotificationDatabase(this@NotificationActivity).checkShowFlag()
-                if (hasUnshown) {//읽지않은 데이터가 있을 경우
-                    Auth.setShowFlag(true)
+                if(!recyclerView.canScrollVertically(1)) {
+                    if(!notificationViewModel.checkLoading()){
+                        getNotification()
+                    }
                 }
-                else{
-                    Auth.setShowFlag(false)
-                }
-                isEnabled = false
-                onBackPressedDispatcher.onBackPressed()
             }
-        }
-        onBackPressedDispatcher.addCallback(this, callback)
+        })
+    }
+
+    private fun getNotification(){
+        notificationViewModel.getNotificationResults()
+    }
+
+    private fun setNoneImage() {
+        binding.NoneImage.visibility = View.VISIBLE
+        binding.NoneText.visibility = View.VISIBLE
+        Log.d("NONE SET", "NONE SET")
+    }
+
+
+    private fun clearNoneImage() {
+        binding.NoneImage.visibility = View.GONE
+        binding.NoneText.visibility = View.GONE
+        Log.d("NONE GONE", "NONE IS GONE")
     }
 }
