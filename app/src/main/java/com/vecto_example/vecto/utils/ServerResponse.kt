e83d1bdddb7c63d4c280dadc97a2e402cf539b9b
joonbo97 <joonbo97@naver.com>
2024-04-21 18:05:25 +0900

    개선

--- app/src/main/java/com/vecto_example/vecto/utils/ServerResponse.kt
+++ app/src/main/java/com/vecto_example/vecto/utils/ServerResponse.kt
@@ -3,12 +3,13 @@ package com.vecto_example.vecto.utils
 enum class ServerResponse(val code:String) {
     /*   SUCCESS   */
 
-    SUCCESS_GETFOLLOW_FOLLOWING("S028"),    //팔로우 상태
-    SUCCESS_GETFOLLOW_UNFOLLOWING("S029"),  //언팔로우 상태
     SUCCESS_POSTFOLLOW("S024"),             //팔로우 등록 성공
     SUCCESS_DELETEFOLLOW("S025"),           //팔로우 해제 성공
     SUCCESS_ALREADY_POSTFOLLOW("S026"),     //팔로우 이미 등록
     SUCCESS_ALREADY_DELETEFOLLOW("S027"),   //팔로우 이미 해제
+    SUCCESS_GETFOLLOW_FOLLOWING("S028"),    //팔로우 상태
+    SUCCESS_GETFOLLOW_UNFOLLOWING("S029"),  //언팔로우 상태
+
 
 
     /*   ERROR   */
