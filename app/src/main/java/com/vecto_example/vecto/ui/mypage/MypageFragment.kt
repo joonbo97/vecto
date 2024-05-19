9b20e916908bd0b1d074dab0d8e6b77d548ed7dd
joonbo97 <joonbo97@naver.com>
2024-05-20 08:45:36 +0900

    로그아웃 추가

--- app/src/main/java/com/vecto_example/vecto/ui/mypage/MypageFragment.kt
+++ app/src/main/java/com/vecto_example/vecto/ui/mypage/MypageFragment.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.vecto_example.vecto.ui.login.LoginActivity
 import com.vecto_example.vecto.ui.main.MainActivity
